cc1008d931c88eadf0e2b721e0608749f1279579
Pritam mann <preetam1202204@gmail.com>
2025-10-13 10:39:44 +0530

    vercel fix

--- frontend/src/Components/Common/Navbar.jsx
+++ frontend/src/Components/Common/Navbar.jsx
@@ -4,8 +4,8 @@ import { useDispatch } from "react-redux";
 import { useAuth } from "../../context/AuthContext";
 import { logout } from "../../store/slices/auth/authSlice";
 import logo from "../../assets/blacklogo.png";
+import AuthModal from "../Auth/Authodal";
 import MobileSidebar from "./MobileSidebar";
-import AuthModal from "../Auth/";
 import {
   FiSearch,
   FiUser,
